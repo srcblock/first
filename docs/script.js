4c20bfaa99e770474ba01bb1b34ec1b01f30a77e
srcblock <nafi@tuta.io>
2024-08-07 17:20:54 +0600

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -58,8 +58,9 @@ function firstSection() {
 
     <p>The animation scenes are rendered from Python scripts using Manim in Jupyter Notebook on Google Colab.</p>
 
-    <img src="https://img.shields.io/badge/Manim_in_Colab-link?style=flat&logo=googlecolab&labelColor=grey&color=blue" style="float:left; margin-right: 5px;>
-    <a href="https://colab.curiouswalk.com/manim">&nbsp;colab.curiouswalk.com/manim</a>
+    <a href="https://colab.curiouswalk.com/manim">
+    <img src="https://img.shields.io/badge/Manim_in_Colab-link?style=flat&logo=googlecolab&labelColor=grey&color=blue">
+    &nbsp;colab.curiouswalk.com/manim</a>
 
 </div>
 `;
